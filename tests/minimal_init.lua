b531a6483edf265c74a08dfa3f7cc747ff63bd98
Douglas M. <douglasmassolari@hotmail.com>
2024-03-08 17:29:08 +0100

    refactor(minimal_init): set rtp inside loadModule

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,20 +1,20 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+append = function(option, value)
+    option.append(option, value)
+end
 loadModule = function(module, source, directory)
     local moduleDir = directory or "/tmp/" .. module
     local directoryExists = vim.fn.isdirectory(moduleDir)
     if directoryExists == 0 then
         vim.fn.system({"git", "clone", source, moduleDir})
     end
+    append(vim.opt.rtp, moduleDir)
     return moduleDir
 end
 plenaryDir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
 loadModule("plenary.nvim", "https://github.com/nvim-lua/plenary.nvim", plenaryDir)
 telescopeDir = loadModule("telescope.nvim", "https://github.com/nvim-telescope/telescope.nvim")
-append = function(option, value)
-    option.append(option, value)
-end
 append(vim.opt.rtp, ".")
-append(vim.opt.rtp, plenaryDir)
-append(vim.opt.rtp, telescopeDir)
 vim.cmd("runtime plugin/plenary.vim")
+vim.cmd("runtime plugin/telescope.lua")
 require("plenary.busted")
